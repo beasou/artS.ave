c678c49732ef15192f1f4cc82d1a88e192b17d8d
beasou <beatrizreginasouza1@gmail.com>
2023-02-07 23:16:26 -0300

    Criando API

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const data = require("./data.json");
+
+app.use(express.json());
+
+app.get("/links", function (request, response) {
+  response.json(data);
+});
+
+app.get("/links/:id", function (request, response) {
+  const { id } = request.params;
+  const link = data.find((link) => link.id == id);
+  if (!link) return response.status(204).json();
+  response.json(link);
+});
+
+app.post("/links", function (request, response) {
+  const { URL, titulo } = request.body;
+
+  response.json({ URL, titulo });
+});
+
+app.put("/links/:id", function (request, response) {
+  const { id } = request.params;
+  const link = data.find((link) => link.id == id);
+  if (!link) return response.status(204).json();
+  const { URL, titulo } = request.body;
+  link.URL = URL;
+  link.titulo = titulo;
+  response.json(link);
+});
+
+app.delete("/links/:id", function (request, response) {
+    const { id } = request.params;
+    const linksFiltered = data.filter(link => link.id != id);
+    response.json(linksFiltered);
+});
+
+app.listen(3003, function () {
+  console.log("Server is running");
+});
